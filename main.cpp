c45c6f116bfefd35a800b07b041ace05c43dc809
Loc Trinh <loctrinh@mit.edu>
2015-12-25 23:09:51 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+Mat src, dst;
+char window_name1[] = "Unprocessed Image";
+char window_name2[] = "Processed Image";
+
+int main( int argc, char** argv )
+{
+    /// Load the source image
+    src = imread( argv[1], 1 );
+
+    if (!src.data){
+        cerr << "ERROR: Image not found." << endl;
+        return 1;
+    }
+
+    namedWindow( window_name1, WINDOW_AUTOSIZE );
+    imshow("Unprocessed Image",src);
+
+    dst = src.clone();
+    GaussianBlur( src, dst, Size( 15, 15 ), 0, 0 );
+
+    namedWindow( window_name2, WINDOW_AUTOSIZE );
+    imshow("Processed Image",dst);
+
+    waitKey();
+    return 0;
+}
